900724ba1108fa9b37526e0952eda5035b39419a
Priyanshu Sharma <priyanshusharma9998@gmail.com>
2026-02-04 21:16:57 +0530

    Update CORS origin for production deployment

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 app.use(express.json({ limit: "4mb" }));
 app.use(
     cors({
-        origin: "http://localhost:5173", // ✅ Vite frontend port
+        origin: "https://chat-app-backend-tz1y.onrender.com", // ✅ Vite frontend port
         credentials: true,               // ✅ allow cookies & headers
     })
 );
